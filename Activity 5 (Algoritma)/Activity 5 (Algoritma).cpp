ae4e6c542dcd4a5edfbabae0e4387f5de5ce8c86
ASUS <faris13abiyu@gmail.com>
2024-04-25 17:29:20 +0700

    while

--- Activity 5 (Algoritma)/Activity 5 (Algoritma).cpp
+++ Activity 5 (Algoritma)/Activity 5 (Algoritma).cpp
@@ -31,5 +31,19 @@ void addNote() {
 		START = nodeBaru;
 		return;
 	}
+
+	Node* previous = START;
+	Node* current = START;
+
+	while ((current != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		previous = current;
+		current = current->next;
+	}
 	
 
